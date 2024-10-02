bf3f0a90c5a105eee02bbb938864bba6342f1d9a
Uzair Ahmed <essani.addy@gmail.com>
2024-10-02 08:18:34 +0500

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div className="homeContainer">
+      <div className="childContainer">
+        Hey,
+        <br />
+        I'm <h1>Uzair Ahmed</h1>
+        <div className="become"> & become a</div>
+        Web Designer
+      </div>
+      <div className="childContainer">
+        <img
+          src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg"
+          alt="Uzair picture Shahow"
+        />
+      </div>
+    </div>
+  );
+}
